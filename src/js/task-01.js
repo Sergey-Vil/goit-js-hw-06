a4196bc1394fc1904e30ee00597f3fc19b63c170
Sergey-Vil <vilchinskiy.s.st@gmail.com>
2022-12-21 09:07:17 +0200

    the solution task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,16 +1,8 @@
 const categoriesEl = [...document.querySelectorAll(".item")];
 console.log(`Number of categories: ${categoriesEl.length}`);
-// console.log(categoriesEl);
 
 const categoriesArr = categoriesEl.map((item) => {
   const itemArr = [...item.children];
   console.log(`Category:${itemArr[0].textContent}`);
   console.log(`Elements:${itemArr[1].children.length}`);
 });
-
-//   itemObj = {};
-// });
-// console.log([...categoria.children]);
-// });
-// const categoriesItemEl = categoriesEl.ch;
-// console.log(categoriesEl.textContent);
