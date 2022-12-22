8de9260ca2fa51b00d92f0fc9f1508f664825940
Sergey-Vil <vilchinskiy.s.st@gmail.com>
2022-12-22 22:31:07 +0200

    task-05

---
+++ src/js/task-05.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", onNameInputInput);
+
+function onNameInputInput(e) {
+  nameOutput.textContent = e.target.value || "Anonymous";
+}
